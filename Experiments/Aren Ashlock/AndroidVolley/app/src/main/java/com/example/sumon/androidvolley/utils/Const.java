0ea74b802330bd28300591029f3dfbbffd7154e0
Aren Ashlock <77025146+arenashlock@users.noreply.github.com>
2023-10-11 20:26:28 -0500

    Adding the experiments folder

---
+++ Experiments/Aren Ashlock/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -0,0 +1,8 @@
+package com.example.sumon.androidvolley.utils;
+
+public class Const {
+	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
+	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
+	public static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
+	public static final String URL_IMAGE = "https://dg.imgix.net/do-you-think-you-re-happy-jgdbfiey-en/landscape/do-you-think-you-re-happy-jgdbfiey-9bb0198eeccd0a3c3c13aed064e2e2b3.jpg?ts=1520525855&ixlib=rails-4.2.0&fit=crop&w=2000&h=1050";
+}
